7aa88d0e77ddbcf80ec184f228ada82b1fe4df06
MaciejNajman <m-najman@wp.pl>
2023-06-07 13:09:28 +0200

    second commit

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ class Projectile {
 		this.position = position
 		this.velocity = velocity
 		
-		this.radius = 3
+		this.radius = 4
 	}
 	
 	draw() {
@@ -79,6 +79,58 @@ class Projectile {
 		c.fillStyle = 'red'
 		c.fill()
 		c.closePath()
+	}
+	
+	update() {
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+	}
+}
+
+class Particle {
+	constructor({position, velocity, radius, color}) {
+		this.position = position
+		this.velocity = velocity
+		
+		this.radius = radius
+		this.color = color
+		this.opacity = 1
+	}
+	
+	draw() {
+		c.save()
+		c.globalAlpha = this.opacity
+		c.beginPath()
+		c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+		c.fillStyle = this.color
+		c.fill()
+		c.closePath()
+		c.restore()
+	}
+	
+	update() {
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+		
+		this.opacity -= 0.01
+	}
+}
+
+class InvaderProjectile {
+	constructor({position, velocity}) {
+		this.position = position
+		this.velocity = velocity
+		
+		this.width = 3
+		this.height = 10
+	}
+	
+	draw() {
+		c.fillStyle = 'white'
+		c.fillRect(this.position.x, this.position.y, this.width,
+		 this.height)
 	}
 	
 	update() {
@@ -129,6 +181,21 @@ class Invader {
 			this.position.x += velocity.x
 			this.position.y += velocity.y
 		}
+	}
+	
+	shoot(invaderProjectiles) {
+		invaderProjectiles.push(
+			new InvaderProjectile({
+				position: {
+					x: this.position.x + this.width / 2,
+					y: this.position.y + this.height
+				},
+				velocity: {
+					x: 0,
+					y: 5
+				}
+			})
+		)
 	}
 }
 
@@ -182,10 +249,14 @@ class Grid {
 
 //stworzenie instancji klasy Player
 const player = new Player()
-//tablica pocisków do animacji i renderu
+//tablica pocisków gracza
 const projectiles = []
 //tablica obiektów klasy Grid dla grup Invaderow
-const grids = [new Grid()]
+const grids = []
+//tablica pocisków Invadera
+const invaderProjectiles = []
+//tablica wybuchow
+const particles = []
 //monitorowanie klawiszy
 const keys = {
 	a: {
@@ -199,12 +270,68 @@ const keys = {
 	}
 }
 
-//petla animacji do rysowania elementów
+let frames = 0
+//losowy przerwa pomiedzy kolejnymi falami Invaderow
+let randomInterval = Math.floor(Math.random() * 500 + 500)
+
+//funkcja do tworzenia wybuchów
+function createParticles({object, color}) {
+	//tworzenie wybuchow
+	for (let i = 0; i < 15; i++) {
+		particles.push(new Particle({
+			position: {
+				x: object.position.x + object.width / 2,
+				y: object.position.y + object.height / 2
+			},
+			velocity: {
+				x: (Math.random() - 0.5) * 2,
+				y: (Math.random() - 0.5) * 2
+			},
+			radius: Math.random() * 3,
+			color: color || '#BAA0DE'
+		}))
+	}
+}
+
+
+//petla animacji do renderowania elementów
 function animate() {
 	requestAnimationFrame(animate)
 	c.fillStyle = 'black'
 	c.fillRect(0, 0, canvas.width, canvas.height)
 	player.update()
+	particles.forEach((particle, i) => {
+		if (particle.opacity <= 0) {
+			setTimeout(() => {
+				particles.splice(i, 1)
+			}, 0)
+		} else {
+			particle.update()
+		}
+	})
+	
+	invaderProjectiles.forEach((invaderProjectile, index) => {
+		if (invaderProjectile.position.y +
+			invaderProjectile.height >= canvas.height) {
+			setTimeout(() => {
+				invaderProjectiles.splice(index, 1)
+			}, 0)	
+		} else {
+			invaderProjectile.update()
+		}
+		
+		if (
+			invaderProjectile.position.y + invaderProjectile.height >=
+			player.position.y &&
+			invaderProjectile.position.x + invaderProjectile.width >=
+			player.position.x &&
+			invaderProjectile.position.x <=
+			player.position.x + player.width
+		) {
+			console.log('you lose')
+		}
+	})
+	
 	projectiles.forEach((projectile, index) => {
 		if(projectile.position.y + projectile.radius <= 0) {
 			setTimeout(() =>{
@@ -215,10 +342,65 @@ function animate() {
 		}
 	})
 	
-	grids.forEach((grid) => {
+	//pętla tworzenie fali Invaderow
+	grids.forEach((grid, gridIndex) => {
 		grid.update()
-		grid.invaders.forEach((invader) => {
+		
+		//tworzenie Invader pociskow
+		if (frames % 100 === 0 && grid.invaders.length > 0) {
+			grid.invaders[Math.floor(Math.random() *
+			 grid.invaders.length)].shoot(invaderProjectiles)
+		}
+			
+		grid.invaders.forEach((invader, i) => {
 			invader.update({velocity: grid.velocity})
+			
+			
+			//pociski trafiają Invaderow
+			projectiles.forEach((projectile, j) => {
+				if (
+					projectile.position.y - projectile.radius <=
+					 invader.position.y + invader.height &&
+					projectile.position.x + projectile.radius >=
+					 invader.position.x && 
+					projectile.position.x - projectile.radius <=
+					 invader.position.x + invader.width &&
+					projectile.position.y + projectile.radius >=
+					 invader.position.y
+				) {
+					
+					setTimeout(() => {
+						const invaderFound = grid.invaders.find(
+						 (invader2) => invader2 === invader
+						)
+						const projectileFound = projectiles.find(
+						 (projectile2) => projectile2 === projectile
+						)
+						
+						//usuń Invaderow i pociski
+						if (invaderFound && projectileFound) {
+							createParticles()
+							
+							grid.invaders.splice(i, 1)
+							projectiles.splice(j, 1)
+							
+							if (grid.invaders.length > 0) {
+								const firstInvader = grid.invaders[0]
+								const lastInvader = grid.invaders[
+								 grid.invaders.length -1]
+								 
+								grid.width = 
+								 lastInvader.position.x -
+								 firstInvader.position.x +
+								 lastInvader.width
+								grid.position.x = firstInvader.position.x
+							} else {
+								grids.splice(gridIndex, 1)
+							}
+						}
+					}, 0)
+				}
+			})
 		})
 	})
 	
@@ -235,6 +417,17 @@ function animate() {
 		player.velocity.x = 0
 		player.rotation = 0
 	}
+	
+	//console.log(frames)
+	// tworzenie fali Invaderow
+	if (frames % randomInterval === 0) {
+		grids.push(new Grid())
+		randomInterval = Math.floor(Math.random() * 500 + 500)
+		frames = 0
+	}
+	
+	
+	frames++
 }
 
 animate()
